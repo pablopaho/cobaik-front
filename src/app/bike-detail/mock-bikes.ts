219eaa32088174f69d02b100147429d357ecfa80
juan diego estrada <jdestradap@gmail.com>
2017-06-05 07:41:37 -0500

    mocking bikes.

---
+++ src/app/bike-detail/mock-bikes.ts
@@ -0,0 +1,14 @@
+import { Bike } from './bike';
+
+export const BIKES: Bike[] = [
+  {name: "trek", reference: "sl 1200"},
+  {name: "trek1", reference: "sl 3400"},
+  {name: "trek2", reference: "sl 3500"},
+  {name: "trek3", reference: "sl 3600"},
+  {name: "trek4", reference: "sl 3700"},
+  {name: "trek5", reference: "sl 3800"},
+  {name: "trek6", reference: "sl 3900"},
+  {name: "trek7", reference: "sl 3000"},
+  {name: "trek8", reference: "sl 3900"},
+  {name: "trek9", reference: "sl 3400"}
+];
