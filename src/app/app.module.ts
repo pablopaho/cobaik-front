e9885dc5119c65b9edebba4bcb5757588342406c
juan diego estrada <jdestradap@gmail.com>
2017-08-01 21:41:58 -0500

    first step integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { HttpModule }    from '@angular/http';
 import { AppComponent } from './app.component';
 
 import { AlertModule } from 'ngx-bootstrap';
+import { HomeComponent } from './home/home.component';
 import { BikeDetailComponent } from './bike-detail/bike-detail.component';
 import { BikeService } from './bike.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     BikeDetailComponent
   ],
   imports: [
