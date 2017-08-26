03485c27e0795defea3873aee1fa923e8d25a07a
juan diego estrada <jdestradap@gmail.com>
2017-08-26 18:33:26 -0500

    fix

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,4 +1,10 @@
 export class Contact {
+  fullname: string;
+  email: string;
+  age: number;
+  residence_city: string;
+  city_to_visit: string;
+  message: string;
 
   constructor(
     fullname: string,
