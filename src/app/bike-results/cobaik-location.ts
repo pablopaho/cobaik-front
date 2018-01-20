34406843db04650340e42896be287eac71d0d483
juan diego estrada <jdestradap@gmail.com>
2018-01-20 12:02:23 -0500

    print on map

---
+++ src/app/bike-results/cobaik-location.ts
@@ -0,0 +1,11 @@
+export class CobaikLocation {
+  latitude: number;
+  longitude: number;
+  city_description: string;
+
+  public constructor(latitude, longitude, city_description) {
+      this.latitude    = latitude;
+      this.longitude   = longitude;
+      city_description = city_description;
+  }
+}
